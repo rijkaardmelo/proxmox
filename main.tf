eeec5a290a17f56487bdb06ca9df165fe4aa0095
Rijkaard Melo <rijkaardmelo@gmail.com>
2024-11-16 22:45:14 -0300

    main

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-variable "ssh_key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcwZAOfqf6E6p8IkrurF2vR3NccPbMlXFPaFe2+Eh/8QnQCJVTL6PKduXjXynuLziC9cubXIDzQA+4OpFYUV2u0fAkXLOXRIwgEmOrnsGAqJTqIsMC3XwGRhR9M84c4XPAX5sYpOsvZX/qwFE95GAdExCUkS3H39rpmSCnZG9AY4nPsVRlIIDP+/6YSy9KWp2YVYe5bDaMKRtwKSq3EOUhl3Mm8Ykzd35Z0Cysgm2hR2poN+EB7GD67fyi+6ohpdJHVhinHi7cQI4DUp+37nVZG4ofYFL9yRdULlHcFa9MocESvFVlVW0FCvwFKXDty6askpg9yf4FnM0OSbhgqXzD root@192.168.31.2"
-}
-
 terraform {
   required_providers {
     proxmox = {
@@ -20,7 +16,7 @@ provider "proxmox" {
 
 resource    "proxmox_vm_qemu"   "SRV-BACULA-DIRECTOR"   {
     name                    =   "SRV-BACULA-DIRECTOR"
-    target_node             =   "proxmox"
+    target_node             =   "pve"
     clone                   =   "debian-12-generic-amd64"
     define_connection_info  =   true
     cpu                     =   "x86-64-v2-AES"
@@ -30,6 +26,8 @@ resource    "proxmox_vm_qemu"   "SRV-BACULA-DIRECTOR"   {
     memory                  =   2048
     onboot                  =   true
     scsihw                  =   "virtio-scsi-single"
+    os_type                 =   "ubuntu"
+    # ipconfig0               =   "ip=192.168.31.5/24,gw=192.168.31.1"
     serial  {
         id      =   0
         type    =   "socket"
@@ -39,7 +37,7 @@ resource    "proxmox_vm_qemu"   "SRV-BACULA-DIRECTOR"   {
             scsi0   {
                 disk    {
                     iothread    =   true
-                    size        =   32
+                    size        =   8
                     storage     =   "local-lvm"
                 }
             }
@@ -50,8 +48,4 @@ resource    "proxmox_vm_qemu"   "SRV-BACULA-DIRECTOR"   {
         bridge  =   "vmbr0"
         tag     =   -1
     }
-    ipconfig0 = "ip=192.168.31.4/24,gw=192.168.1.1"
-    sshkeys = <<EOF
-    ${var.ssh_key}
-    EOF 
 }
